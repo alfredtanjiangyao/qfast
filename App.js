d62424c9ebe87a22a31f919459c4c96358946ffa
alfredtanjiangyao <alfredtanjiangyao@gmail.com>
2023-05-29 00:06:13 +0800

    <description>

--- App.js
+++ App.js
@@ -1,14 +1,71 @@
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState } from 'react';
+// import { KeyboardAvoidingView ,SafeAreaView, TouchableOpacity, TextInput, StyleSheet, Text, View, Image} from 'react-native';
+// import  SignIn from './SignIn.js';
+
+
+// export default function App() {
+//   return (
+//     <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+//       <SignIn />
+//     </KeyboardAvoidingView>
+//   );
+// }
+
+// App.js
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/screens/login';
+import Signup from './src/screens/signup';
+import Dashboard from './src/screens/dashboard';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { KeyboardAvoidingView ,SafeAreaView, TouchableOpacity, TextInput, StyleSheet, Text, View, Image} from 'react-native';
-import  SignIn from './SignIn.js';
 
-
-export default function App() {
+const Stack = createStackNavigator();
+function MyStack() {
   return (
-    <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
-      <SignIn />
-    </KeyboardAvoidingView>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={[
+          { title: 'Login' },
+          { headerLeft: null }
+        ]}
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={[
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       ]}
+      />
+    </Stack.Navigator>
   );
 }
-  
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
