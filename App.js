622e939bfa27d68c6b153cb25fc296797364a904
alfredtanjiangyao <alfredtanjiangyao@gmail.com>
2023-07-23 15:40:56 +0800

    arrange folder

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 // App.js
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/screens/login';
-import Signup from './src/screens/signup';
-import Dashboard from './src/screens/dashboard';
-import 'firebase/auth';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/screens/login";
+import Signup from "./src/screens/signup";
+import Dashboard from "./src/screens/dashboard";
+import "firebase/auth";
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import {
@@ -20,17 +20,20 @@ import {
 } from "react-native";
 import Booking from "./booking";
 import Home from "./staff page/home";
-import Register from "./staff page/register";;
-import Edit from './edit';
-import StaffView from './staff page/view';
-import Profile from './profile';
-import BirthdatePicker from './BirthdatePicker';
+import Register from "./staff page/register";
+import Edit from "./src/screens/user page/edit";
+import StaffView from "./staff page/view";
+import Profile from "./src/screens/user page/profile";
+import BirthdatePicker from "./src/screens/user page/BirthdatePicker";
 
-import * as BackgroundFetch from 'expo-background-fetch';
-import { registerTaskAsync, BackgroundFetchResult } from 'expo-background-fetch';
-import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from "expo-background-fetch";
+import {
+  registerTaskAsync,
+  BackgroundFetchResult,
+} from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager";
 
-const BACKGROUND_FETCH_TASK = 'background-fetch';
+const BACKGROUND_FETCH_TASK = "background-fetch";
 
 const Stack = createStackNavigator();
 
@@ -40,8 +43,6 @@ const Stack = createStackNavigator();
 // BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
 //   minimumInterval: 10, // The minimum time interval (in seconds) for the background task to run (e.g., every 60 seconds)
 // });
-
-
 
 // BackgroundFetch.setMinimumIntervalAsync(10);
 
@@ -79,28 +80,26 @@ const Stack = createStackNavigator();
 //   minimumInterval: 10, // The minimum time interval (in seconds) for the background task to run (e.g., every 60 seconds)
 // });
 
-
 // const backgroundTask = async (taskData) => {
-  // try {
-  //   const user = auth.currentUser;
-  //   if (user && !user.emailVerified) {
-  //     // Perform the email verification check here
-  //     await user.reload();
-  //     if (user.emailVerified) {
-  //       // Email is verified, update the UI or take necessary actions
-  //       console.log('Email is verified.');
-  //     }
-  //   }
+// try {
+//   const user = auth.currentUser;
+//   if (user && !user.emailVerified) {
+//     // Perform the email verification check here
+//     await user.reload();
+//     if (user.emailVerified) {
+//       // Email is verified, update the UI or take necessary actions
+//       console.log('Email is verified.');
+//     }
+//   }
 
-  //   return BackgroundFetchResult.NewData;
-  // } catch (error) {
-  //   console.error('Background task error:', error);
-  //   return BackgroundFetchResult.Failed;
-  // }
+//   return BackgroundFetchResult.NewData;
+// } catch (error) {
+//   console.error('Background task error:', error);
+//   return BackgroundFetchResult.Failed;
+// }
 // };
 
 function MyStack() {
-
   return (
     <Stack.Navigator
       initialRouteName="Login"
@@ -131,33 +130,17 @@ function MyStack() {
         component={Dashboard}
         options={[{ title: "Dashboard" }, { headerLeft: null }]}
       />
-      <Stack.Screen
-        name="Booking"
-        component={Booking} />
-      <Stack.Screen
-        name="Register"
-        component={Register} />
-      <Stack.Screen
-        name="Home"
-        component={Home} />
-      <Stack.Screen
-        name="Edit"
-        component={Edit} />
-      <Stack.Screen
-        name="StaffView"
-        component={StaffView} />
-        <Stack.Screen
-        name="Profile"
-        component={Profile} />
-      <Stack.Screen
-        name="BirthdatePicker"
-        component={BirthdatePicker} />
+      <Stack.Screen name="Booking" component={Booking} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Edit" component={Edit} />
+      <Stack.Screen name="StaffView" component={StaffView} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="BirthdatePicker" component={BirthdatePicker} />
     </Stack.Navigator>
   );
 }
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <MyStack />
